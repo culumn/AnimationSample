f702327c6ae3dc15cd0f28a6cf79612d5592d15b
culumn <yoxtsushi.44106@gmail.com>
2018-02-17 15:17:19 +0900

    Init .xcodeproj

---
+++ AnimationSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimationSample
+//
+//  Created by Matsuoka Yoshiteru on 2018/02/17.
+//  Copyright © 2018年 com.github.culumn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
