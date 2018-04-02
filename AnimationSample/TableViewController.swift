45be3f717282681b0b17c1aa4942305114aaea90
culumn <yoxtsushi.44106@gmail.com>
2018-04-02 21:48:16 +0900

    add tableview cell animation

--- AnimationSample/TableViewController.swift
+++ AnimationSample/TableViewController.swift
@@ -8,88 +8,81 @@
 
 import UIKit
 
-class TableViewController: UITableViewController {
+final class TableViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+// MARK: - UITableViewDataSource
+extension TableViewController: UITableViewDataSource {
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 200
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ScrollCell",
+                                                 for: indexPath)
+        cell.textLabel?.text = String(describing: indexPath)
         return cell
     }
-    */
+}
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
+// MARK: - UITableViewDelegate
+extension TableViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        guard let firstCell = tableView.visibleCells.first,
+            let firstIndex = tableView.indexPath(for: firstCell),
+            let lastCell = tableView.visibleCells.last,
+            let lastIndex = tableView.indexPath(for: lastCell)
+            else { return }
+
+        let translationX = cell.bounds.width / 6
+        let translationY: CGFloat = {
+            if indexPath.row > lastIndex.row {
+                return cell.bounds.height / 2
+            } else if indexPath.row < firstIndex.row {
+                return cell.bounds.height / -2
+            }
+            return 0
+        }()
+
+        cell.transform = CGAffineTransform(translationX: translationX,
+                                           y: translationY)
+        cell.alpha = 0.0
+
+        UIView.animate(withDuration: 0.3,
+                       delay: 0.0,
+                       options: [.preferredFramesPerSecond60,
+                                 .curveEaseOut],
+                       animations: {
+                        cell.transform = CGAffineTransform.identity
+                        cell.alpha = 1.0
+        }, completion: nil)
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
+        guard let cell = tableView.cellForRow(at: indexPath) else { preconditionFailure() }
+        UIView.animate(withDuration: 0.15) {
+            cell.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+        }
         return true
     }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    func tableView(_ tableView: UITableView, didUnhighlightRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) else { preconditionFailure() }
+        UIView.animate(withDuration: 0.15) {
+            cell.transform = CGAffineTransform.identity
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
